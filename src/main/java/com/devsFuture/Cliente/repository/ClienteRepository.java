e2eb91a881f24c6ea6ec58bc2f09ecbe0c7cb9c9
Willer Andrade <willer10gabriel@gmail.com>
2023-05-11 22:33:17 -0300

    Criado Repositório de Cliente

---
+++ src/main/java/com/devsFuture/Cliente/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.devsFuture.Cliente.repository;
+
+import com.devsFuture.Cliente.entities.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
+}
